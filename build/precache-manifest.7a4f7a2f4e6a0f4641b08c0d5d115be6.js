ee00fdbf0e2a5590a41e20054dd3de559e1e1bc4
Michal Karol <michal.p.karol@gmail.com>
2020-03-14 19:12:41 +0100

    new GH-pages build

---
+++ build/precache-manifest.7a4f7a2f4e6a0f4641b08c0d5d115be6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2bc0239c452b4a9bd1d3394d89a2a64",
+    "url": "/coronavirus/index.html"
+  },
+  {
+    "revision": "5cd07eafe900ada01f95",
+    "url": "/coronavirus/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "1bf931478e3366ae35f1",
+    "url": "/coronavirus/static/css/main.d600407a.chunk.css"
+  },
+  {
+    "revision": "5cd07eafe900ada01f95",
+    "url": "/coronavirus/static/js/2.9638e2e5.chunk.js"
+  },
+  {
+    "revision": "9c32f827cc2f2844d7415f2f3c9aab28",
+    "url": "/coronavirus/static/js/2.9638e2e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bf931478e3366ae35f1",
+    "url": "/coronavirus/static/js/main.8a4ff590.chunk.js"
+  },
+  {
+    "revision": "28e88e3281a1faf32744",
+    "url": "/coronavirus/static/js/runtime-main.8190772f.js"
+  },
+  {
+    "revision": "ce63c0459a272d8301b186ec73db37e7",
+    "url": "/coronavirus/static/media/check-circle.ce63c045.svg"
+  },
+  {
+    "revision": "e316d44d173b5c10df022722cbd9eeb3",
+    "url": "/coronavirus/static/media/heart.e316d44d.svg"
+  },
+  {
+    "revision": "8a34b88dea4859ce46416e71841f00be",
+    "url": "/coronavirus/static/media/link.8a34b88d.svg"
+  },
+  {
+    "revision": "c89cb7d970a5354712a70d3cb3225308",
+    "url": "/coronavirus/static/media/x-circle.c89cb7d9.svg"
+  }
+]);
