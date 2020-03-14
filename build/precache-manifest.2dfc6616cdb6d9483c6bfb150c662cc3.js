b477f8eb18b8f56755899ec75ff7427cc27284f4
Michal Karol <michal.p.karol@gmail.com>
2020-03-14 18:39:04 +0100

    Initial build subtree commit

---
+++ build/precache-manifest.2dfc6616cdb6d9483c6bfb150c662cc3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc03c7d5e115cfb82d871c64edb386d",
+    "url": "/koronawiruswpl.pl/index.html"
+  },
+  {
+    "revision": "5cd07eafe900ada01f95",
+    "url": "/koronawiruswpl.pl/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "b854369d62d9fee7d213",
+    "url": "/koronawiruswpl.pl/static/css/main.d600407a.chunk.css"
+  },
+  {
+    "revision": "5cd07eafe900ada01f95",
+    "url": "/koronawiruswpl.pl/static/js/2.9638e2e5.chunk.js"
+  },
+  {
+    "revision": "9c32f827cc2f2844d7415f2f3c9aab28",
+    "url": "/koronawiruswpl.pl/static/js/2.9638e2e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b854369d62d9fee7d213",
+    "url": "/koronawiruswpl.pl/static/js/main.c1b616b9.chunk.js"
+  },
+  {
+    "revision": "cdc4f69e4e678a517c9c",
+    "url": "/koronawiruswpl.pl/static/js/runtime-main.05425412.js"
+  },
+  {
+    "revision": "ce63c0459a272d8301b186ec73db37e7",
+    "url": "/koronawiruswpl.pl/static/media/check-circle.ce63c045.svg"
+  },
+  {
+    "revision": "e316d44d173b5c10df022722cbd9eeb3",
+    "url": "/koronawiruswpl.pl/static/media/heart.e316d44d.svg"
+  },
+  {
+    "revision": "8a34b88dea4859ce46416e71841f00be",
+    "url": "/koronawiruswpl.pl/static/media/link.8a34b88d.svg"
+  },
+  {
+    "revision": "c89cb7d970a5354712a70d3cb3225308",
+    "url": "/koronawiruswpl.pl/static/media/x-circle.c89cb7d9.svg"
+  }
+]);
